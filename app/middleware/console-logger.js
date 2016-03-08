3c92a65378900c478a7e362cbc729d1aaec1422d
Nicholas Sofroniew <sofroniewn@janelia.hhmi.org>
2016-03-07 22:57:59 -0500

    try to add file logging

--- app/middleware/console-logger.js
+++ app/middleware/console-logger.js
@@ -3,9 +3,11 @@ var now = require('performance-now')
 const logger = store => next => action => {
   var time = now()
   var result = next(action)
+  var duration = now()-time
   var obj = {'time': time.toFixed(2),
     'action': action,
-    'state': store.getState()
+    'state': store.getState(),
+    'duration': duration.toFixed(3)
     }
   console.log(JSON.stringify(obj))
   return result
